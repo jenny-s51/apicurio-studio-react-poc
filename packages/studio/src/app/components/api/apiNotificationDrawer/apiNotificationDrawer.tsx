2b61313138c0722f5397010929b24618d5098270
jenny-s51 <jshandel12@gmail.com>
2020-04-15 12:37:50 -0400

    adds conditional for when there is no recent data

--- packages/studio/src/app/components/api/apiNotificationDrawer/apiNotificationDrawer.tsx
+++ packages/studio/src/app/components/api/apiNotificationDrawer/apiNotificationDrawer.tsx
@@ -1,44 +1,67 @@
-import React from 'react';
-import { DataList, DataListItem, DataListItemRow, DataListItemCells, DataListCell, DrawerHead, DrawerPanelBody, DrawerPanelContent, Title } from '@patternfly/react-core';
-import { GlobalContext } from '../../../../context';
-import { ApiActivityItem } from '../apiActivityItem/apiActivityItem';
+import React from "react";
+import {
+  DataList,
+  DataListItem,
+  DataListItemRow,
+  DataListItemCells,
+  DataListCell,
+  DrawerHead,
+  DrawerPanelBody,
+  DrawerPanelContent,
+  Title,
+  Text,
+  TextVariants
+} from "@patternfly/react-core";
+import { GlobalContext } from "../../../../context";
+import { ApiActivityItem } from "../apiActivityItem/apiActivityItem";
 
 export const ApiNotificationDrawer = () => {
-    const { recentActivity } = { ... React.useContext(GlobalContext).store }
+  const { recentActivity } = { ...React.useContext(GlobalContext).store };
+  console.log(recentActivity);
 
-    return (
-        <DrawerPanelContent>
-            <DrawerHead>
-                <Title size="lg" headingLevel="h3">
-                    Recent Activity
-                </Title>
-            </DrawerHead>
-            <DrawerPanelBody noPadding>
-                <DataList aria-label="app-data-list-notification-drawer" key="notification-drawer">
-                    <React.Fragment>
-                        { recentActivity.map((activity, index) =>
-                            <DataListItem aria-labelledby={`data-list-item-${index}`} key={index}>
-                                <DataListItemRow>
-                                <DataListItemCells
-                                    dataListCells={[
-                                        <DataListCell key="1">
-                                            <ApiActivityItem
-                                                activityApiName={activity.apiName}
-                                                activityType={activity.type}
-                                                activityOn={activity.on}
-                                                activityData={activity.data}
-                                            />
-                                        </DataListCell>
-                                    ]}
-                                    />
-                                </DataListItemRow>
-                            </DataListItem>
-                        )}
-                    </React.Fragment>
-                </DataList>
-            </DrawerPanelBody>
-        </DrawerPanelContent>
-    )
+  return (
+    <DrawerPanelContent>
+      <DrawerHead>
+        <Title size="lg" headingLevel="h3">
+          Recent Activity
+        </Title>
+      </DrawerHead>
+      <DrawerPanelBody noPadding>
+        <DataList
+          aria-label="app-data-list-notification-drawer"
+          key="notification-drawer"
+        >
+          {recentActivity.length === 0 ? (
+            <Text component={TextVariants.p}>No recent data</Text>
+          ) : (
+            <React.Fragment>
+              {recentActivity.map((activity, index) => (
+                <DataListItem
+                  aria-labelledby={`data-list-item-${index}`}
+                  key={index}
+                >
+                  <DataListItemRow>
+                    <DataListItemCells
+                      dataListCells={[
+                        <DataListCell key="1">
+                          <ApiActivityItem
+                            activityApiName={activity.apiName}
+                            activityType={activity.type}
+                            activityOn={activity.on}
+                            activityData={activity.data}
+                          />
+                        </DataListCell>
+                      ]}
+                    />
+                  </DataListItemRow>
+                </DataListItem>
+              ))}
+            </React.Fragment>
+          )}
+        </DataList>
+      </DrawerPanelBody>
+    </DrawerPanelContent>
+  );
 };
 
 export default ApiNotificationDrawer;
